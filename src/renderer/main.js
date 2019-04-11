257196a7aad066cbe283c32b41589e78981e85ee
hilanmiao <hilanmiao@126.com>
2019-04-11 17:51:43 +0800

    import ui framework Vuetify

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,11 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify' // 引入Vuetify
+import 'vuetify/dist/vuetify.min.css' // 引入Vuetify的css文件
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // 引入Material Desgin Icon的css文件
+
+Vue.use(Vuetify)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
