7b9a142cf70da9ba9a3d8ff26835b1ead46598e9
hilanmiao <hilanmiao@126.com>
2019-05-31 09:28:27 +0800

    import element-ui component

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -8,8 +8,11 @@ import store from './store'
 import Vuetify from 'vuetify' // 引入Vuetify
 import 'vuetify/dist/vuetify.min.css' // 引入Vuetify的css文件
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // 引入Material Desgin Icon的css文件
+import {Upload, Button} from 'element-ui'
 
 Vue.use(Vuetify)
+Vue.use(Button)
+Vue.use(Upload)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
--- src/renderer/views/IncomeAndExpenditure/IncomeAndExpenditure.vue
+++ src/renderer/views/IncomeAndExpenditure/IncomeAndExpenditure.vue
@@ -177,6 +177,21 @@
                                     <v-text-field label="Remark"
                                                   v-model="editedItem.remark"></v-text-field>
                                 </v-flex>
+                                <v-flex xs12>
+                                    <el-upload
+                                            class="upload-demo"
+                                            action="https://jsonplaceholder.typicode.com/posts/"
+                                            :on-preview="handlePreview"
+                                            :on-remove="handleRemove"
+                                            :before-remove="beforeRemove"
+                                            multiple
+                                            :on-exceed="handleExceed"
+                                            :file-list="fileList">
+                                        <!--<el-button size="small" type="primary">点击上传</el-button>-->
+                                        <v-btn color="blue darken-1">Upload</v-btn>
+                                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                                    </el-upload>
+                                </v-flex>
                             </v-layout>
                         </v-container>
                         <small>*indicates required field</small>
@@ -249,6 +264,13 @@
     export default {
         data() {
             return {
+                fileList: [{
+                    name: 'food.jpeg',
+                    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+                }, {
+                    name: 'food2.jpeg',
+                    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+                }],
                 // 表格相关
                 loading: true,
                 showNoData: false,
@@ -346,6 +368,20 @@
             })
         },
         methods: {
+
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            handleExceed(files, fileList) {
+
+            },
+            beforeRemove(file, fileList) {
+
+            },
+
             toggleAll() {
                 if (this.selected.length) this.selected = []
                 else this.selected = this.desserts.slice()
