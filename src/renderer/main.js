bc7c2214dd7d4b4dc9cac2e013c1b0fdb0688261
hilanmiao <hilanmiao@126.com>
2019-12-17 20:47:54 +0800

    Troubleshoot which file caused the compilation error

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,4 +1,4 @@
-import devtools from '@vue/devtools'
+// import devtools from '@vue/devtools'
 import Vue from 'vue'
 import axios from 'axios'
 
@@ -6,13 +6,13 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify' // 引入Vuetify
-import 'vuetify/dist/vuetify.min.css' // 引入Vuetify的css文件
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // 引入Material Desgin Icon的css文件
-import {Upload, Button} from 'element-ui'
+// import 'vuetify/dist/vuetify.min.css' // 引入Vuetify的css文件
+// import 'material-design-icons-iconfont/dist/material-design-icons.css' // 引入Material Desgin Icon的css文件
+// import {Upload, Button} from 'element-ui'
 
 Vue.use(Vuetify)
-Vue.use(Button)
-Vue.use(Upload)
+// Vue.use(Button)
+// Vue.use(Upload)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
